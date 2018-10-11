65c347fa8a32495173e0f491048b2e4410ec8e8e
unknown <Slavik@nif-aa040.ent.ukrgas.com.ua>
2018-10-11 10:21:04 +0300

    calk11.10.18.

---
+++ 1.js
@@ -0,0 +1,20 @@
+var tablo = $('#tablo');
+//var num1 = $('#num1');
+var num2 = $('#num2');
+var num3 = $('#num3');
+var num4 = $('#num4');
+var num5 = $('#num5');
+var num6 = $('#num6');
+var num7 = $('#num7');
+var num8 = $('#num8');
+var num9 = $('#num9');
+var num0 = $('#num0');
+var plus = $('#plus');
+var minus = $('#minus');
+var mnosh = $('#mnosh');
+var dill = $('#dill');
+var dor = $('#dor');
+
+$('button').on('click', function() {
+console.log(button.value);
+});
