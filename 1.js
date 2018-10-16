7e153fcf9ac5e62f0209a03bc2af5b7515981210
Yaroslav_Virstiuk <yarovji@gmail.com>
2018-10-16 14:34:05 +0300

    done

--- 1.js
+++ 1.js
@@ -1,21 +1,27 @@
-var tablo = $('#tablo');
-//var num1 = $('#num1');
-var num2 = $('#num2');
-var num3 = $('#num3');
-var num4 = $('#num4');
-var num5 = $('#num5');
-var num6 = $('#num6');
-var num7 = $('#num7');
-var num8 = $('#num8');
-var num9 = $('#num9');
-var num0 = $('#num0');
-var plus = $('#plus');
-var minus = $('#minus');
-var mnosh = $('#mnosh');
-var dill = $('#dill');
-var dor = $('#dor');
 
-$('button').on('click', function() {
-$('#tablo').text($(this).attr('value'));
+
+$('input').click(function znachvalue() {
+	var num = $(this).attr('value');
+		function insert() {
+		    document.form.display.value=document.form.display.value+num;
+	    }
+	insert()
 });
+
+$('#delit').click(function () {
+	document.form.display.value="";
+	    });
+
+$('#dor').click(function () {
+	document.form.display.value=eval(document.form.display.value);
+	    });
+
+$('#Backspace').click(function () {
+	document.form.display.value=document.form.display.value.substring(0,document.form.display.value.length-1);
+	    });
+
+
+
+
+
 
